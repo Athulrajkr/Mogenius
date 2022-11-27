d578bb626f5d241a9db90f27444c4a0e9c7eb62b
MyBotzz <119148554+MyBotzz@users.noreply.github.com>
2022-11-28 00:59:36 +0530

    Update info.py

--- info.py
+++ info.py
@@ -24,7 +24,7 @@ USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
 PICS = (environ.get('PICS', 'https://telegra.ph/file/527d5a54f1f00550b3a33.jpg https://telegra.ph/file/74a62e699dd51fc460554.jpg https://telegra.ph/file/1ac1de663c0fe7c374fc4.jpg https://telegra.ph/file/7ebaffbc302ce4d7064be.jpg https://telegra.ph/file/ef004a51dcd951304b4fc.jpg https://telegra.ph/file/328006fa71706144c9be3.jpg https://telegra.ph/file/08d393751800837052229.jpg https://telegra.ph/file/7ff750b055c82ade3c178.jpg https://telegra.ph/file/d2bdfb8ede89b6fdd9938.jpg https://telegra.ph/file/8048d1e75dc9c2a473e1b.jpg https://telegra.ph/file/061b6e8fb9890d920fe04.jpg https://telegra.ph/file/339eecfdbaaced87b959c.jpg')).split()
 NOR_IMG = environ.get("NOR_IMG", "https://telegra.ph/file/f2ac735598931279c9fdc.jpg")
 MELCOW_VID = environ.get("MELCOW_VID", "https://telegra.ph/file/4d060a7d48b1fc60579b4.mp4")
-SPELL_IMG = environ.get("SPELL_IMG", "https://telegra.ph/file/a70efdcc7a615dd58e0ca.jpg")
+SPELL_IMG = environ.get("SPELL_IMG", "https://telegra.ph/file/8ea7d3d16ef436935d70e.jpg")
 
 # Admins, Channels & Users
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '').split()]
